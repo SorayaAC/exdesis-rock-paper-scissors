80669765b9e47adf675fd83d0e7c55363be20b93
SorayaAC <soraya.alonso@singularfactory.com>
2022-10-23 23:16:49 +0200

    Add changes

--- src/app/views/game.jsx
+++ src/app/views/game.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import '.././assets/styles/views/game.css';
 import { gameOptions } from '../constants/gameOptions';
